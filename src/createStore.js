e401a8a6832adf35ffea758a431c3ff7fd51ae6e
Yisrael Gurkow <yisrelg3@gmail.com>
2021-07-19 09:36:48 -0400

    commit tree 390a5d0cbf17825ded445dc86b706905b0678ca5

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,23 @@
-let state;
+function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { 
+    dispatch,
+    getState
+  };
+}
+
+let store = createStore(reducer);
+store.dispatch({ type: "@@INIT" }); 
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
@@ -10,19 +29,13 @@ function reducer(state = { count: 0 }, action) {
   }
 }
 
-function dispatch(action) {
-  state = reducer(state, action);
-  render();
-}
-
 function render() {
   let container = document.getElementById("container");
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 }
 
-dispatch({ type: "@@INIT" });
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  dispatch({ type: "counter/increment" });
+  store.dispatch({ type: "counter/increment" });
 });
